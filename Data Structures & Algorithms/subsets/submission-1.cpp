54f16a908d246e29dd83d8277460523d44a7482d
StandardJohn <132251604+StandardJohn@users.noreply.github.com>
2026-08-10 17:23:37 +0800

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> subset;
+        dfs(res, nums, 0, subset);
+        return res;
+    }
+    void dfs(vector<vector<int>>& res, vector<int>& nums, int i, vector<int>& subset) {
+        if (i >= nums.size()) {
+            res.push_back(subset);
+            return;
+        }
+        subset.push_back(nums[i]);
+        dfs(res, nums, i + 1, subset);
+        subset.pop_back();
+        dfs(res, nums, i + 1, subset);
+    }
+};
