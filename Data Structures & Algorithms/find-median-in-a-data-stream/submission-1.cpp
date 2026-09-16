b1165cebb01b55cce67ea62400f358ee8f5750de
StandardJohn <132251604+StandardJohn@users.noreply.github.com>
2026-09-16 11:12:30 +0800

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.cpp
@@ -0,0 +1,38 @@
+class MedianFinder {
+public:
+    priority_queue<int> left;
+    priority_queue<int, vector<int>, greater<int>> right;
+
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        if (left.empty() || (!left.empty() && num < left.top())) {
+            left.push(num);
+        }
+        else {
+            right.push(num);
+        }
+        size_t diff = left.size() > right.size() ? left.size() - right.size() : right.size() - left.size();
+        if (!left.empty() && left.size() > right.size() && diff > 1) {
+            right.push(left.top());
+            left.pop();
+        }
+        else if (!right.empty() && right.size() - left.size() && diff > 1) {
+            left.push(right.top());
+            right.pop();
+        }
+    }
+    
+    double findMedian() {
+        if (left.empty() && right.empty()) 
+            return 0.0;
+        if ((left.size() + right.size()) % 2 == 0)
+            return (double) (left.top() + right.top()) / 2;
+        if (left.empty() || right.size() > left.size())
+            return (double) right.top();
+        if (right.empty() || left.size() < right.size())
+            return (double) left.top();
+    }
+};
