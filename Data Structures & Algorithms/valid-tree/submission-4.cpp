7f125b2a9d87193bfa7c5276625a4126af1108e1
StandardJohn <132251604+StandardJohn@users.noreply.github.com>
2026-09-25 17:36:41 +0800

    Add: valid-tree - submission-4

---
+++ Data Structures & Algorithms/valid-tree/submission-4.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool dfs(vector<bool>& visited, vector<vector<int>>& adj, int& u, int& v) {
+        if (visited[v]) 
+            return true;
+        visited[v] = true;
+        for (int i = 0; i < adj[v].size(); i++) {
+            // cout << v << " " << adj[v][i] << endl;
+            if (visited[adj[v][i]] && adj[v][i] != u || !dfs(visited, adj, v, adj[v][i]))
+                return false;
+        }
+        return true;
+    }
+    bool validTree(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        vector<bool> visited(n, 0);
+        for (int i = 0; i < edges.size(); i++) {
+            int a = edges[i][0], b = edges[i][1];
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+        }
+        int t1 = -1, t2 = 0;
+        if (!dfs(visited, adj, t1, t2))
+            return false;
+        for (bool f : visited) {
+            if (!f) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
